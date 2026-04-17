5bc534aa91f3399b11d1ece3f59fc5958735df45
abhi-deotech <abhimanyu@deotechsolutions.com>
2026-04-17 13:29:53 +0530

    Music App update

--- src/data/musicData.js
+++ src/data/musicData.js
@@ -1,4 +1,15 @@
 export const MUSIC_DATA = [
+  // CHILL/SAFE (Permissive Embeds)
+  {
+    id: 'lofi-beats',
+    youtubeId: 'jfKfPfyJRdk',
+    title: 'Lofi Study Radio',
+    artist: 'Lofi Girl',
+    album: 'Lumina Study Session',
+    cover: 'https://img.youtube.com/vi/jfKfPfyJRdk/hqdefault.jpg',
+    duration: 3600,
+    genre: 'Indie'
+  },
   // TRAP & HIP HOP
   {
     id: 'niagara-falls',
@@ -6,7 +17,7 @@ export const MUSIC_DATA = [
     title: 'Niagara Falls (Foot or 2)',
     artist: 'Metro Boomin, Travis Scott, 21 Savage',
     album: 'HEROES & VILLAINS',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/v9l4yv3w-0w/hqdefault.jpg',
+    cover: 'https://img.youtube.com/vi/v9l4yv3w-0w/hqdefault.jpg',
     duration: 207,
     genre: 'Trap'
   },
@@ -16,7 +27,7 @@ export const MUSIC_DATA = [
     title: 'FE!N',
     artist: 'Travis Scott ft. Playboi Carti',
     album: 'UTOPIA',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/B9S_G3vJjYQ/hqdefault.jpg',
+    cover: 'https://img.youtube.com/vi/B9S_G3vJjYQ/hqdefault.jpg',
     duration: 191,
     genre: 'Trap'
   },
@@ -26,7 +37,7 @@ export const MUSIC_DATA = [
     title: 'Not Like Us',
     artist: 'Kendrick Lamar',
     album: 'Single',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/hB6UToY67-4/hqdefault.jpg',
+    cover: 'https://img.youtube.com/vi/hB6UToY67-4/hqdefault.jpg',
     duration: 274,
     genre: 'Hip Hop'
   },
@@ -36,7 +47,7 @@ export const MUSIC_DATA = [
     title: 'Mask Off',
     artist: 'Future',
     album: 'FUTURE',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/xvZqHgFz51I/hqdefault.jpg',
+    cover: 'https://img.youtube.com/vi/xvZqHgFz51I/hqdefault.jpg',
     duration: 204,
     genre: 'Trap'
   },
@@ -46,7 +57,7 @@ export const MUSIC_DATA = [
     title: 'Magnolia',
     artist: 'Playboi Carti',
     album: 'Playboi Carti',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/f0M_Ff2iKCc/hqdefault.jpg',
+    cover: 'https://img.youtube.com/vi/f0M_Ff2iKCc/hqdefault.jpg',
     duration: 181,
     genre: 'Trap'
   },
@@ -56,7 +67,7 @@ export const MUSIC_DATA = [
     title: 'pushin P',
     artist: 'Gunna, Future, ft. Young Thug',
     album: 'DS4Ever',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/T8XmN_YI7v0/hqdefault.jpg',
+    cover: 'https://img.youtube.com/vi/T8XmN_YI7v0/hqdefault.jpg',
     duration: 136,
     genre: 'Trap'
   },
@@ -66,8 +77,38 @@ export const MUSIC_DATA = [
     title: 'Too Many Nights',
     artist: 'Metro Boomin, Don Toliver, Future',
     album: 'HEROES & VILLAINS',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/O6v7vX3v3v0/hqdefault.jpg',
+    cover: 'https://img.youtube.com/vi/O6v7vX3v3v0/hqdefault.jpg',
     duration: 199,
+    genre: 'Trap'
+  },
+  {
+    id: 'humble',
+    youtubeId: 'tvTRZJ-4EyI',
+    title: 'HUMBLE.',
+    artist: 'Kendrick Lamar',
+    album: 'DAMN.',
+    cover: 'https://img.youtube.com/vi/tvTRZJ-4EyI/hqdefault.jpg',
+    duration: 177,
+    genre: 'Hip Hop'
+  },
+  {
+    id: 'gods-plan',
+    youtubeId: 'xpVfcZ0ZcFM',
+    title: "God's Plan",
+    artist: 'Drake',
+    album: 'Scorpion',
+    cover: 'https://img.youtube.com/vi/xpVfcZ0ZcFM/hqdefault.jpg',
+    duration: 198,
+    genre: 'Hip Hop'
+  },
+  {
+    id: 'sicko-mode',
+    youtubeId: 'd-JBBNg8YKs',
+    title: 'SICKO MODE',
+    artist: 'Travis Scott',
+    album: 'ASTROWORLD',
+    cover: 'https://img.youtube.com/vi/d-JBBNg8YKs/hqdefault.jpg',
+    duration: 312,
     genre: 'Trap'
   },
 
@@ -78,7 +119,7 @@ export const MUSIC_DATA = [
     title: 'Take Me Back To LA',
     artist: 'The Weeknd',
     album: 'Hurry Up Tomorrow',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/z-3I9N99B9gQ/hqdefault.jpg',
+    cover: 'https://img.youtube.com/vi/z-3I9N99B9gQ/hqdefault.jpg',
     duration: 240,
     genre: 'R&B'
   },
@@ -88,7 +129,7 @@ export const MUSIC_DATA = [
     title: 'Snooze',
     artist: 'SZA',
     album: 'SOS',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/L7S6mHn2Vlo/hqdefault.jpg',
+    cover: 'https://img.youtube.com/vi/L7S6mHn2Vlo/hqdefault.jpg',
     duration: 201,
     genre: 'R&B'
   },
@@ -98,7 +139,7 @@ export const MUSIC_DATA = [
     title: 'Passionfruit',
     artist: 'Drake',
     album: 'More Life',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/N9bKBAA22Go/hqdefault.jpg',
+    cover: 'https://img.youtube.com/vi/N9bKBAA22Go/hqdefault.jpg',
     duration: 298,
     genre: 'R&B'
   },
@@ -108,29 +149,121 @@ export const MUSIC_DATA = [
     title: 'telepatía',
     artist: 'Kali Uchis',
     album: 'Sin Miedo',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/bnVUHWCynig/hqdefault.jpg',
+    cover: 'https://img.youtube.com/vi/bnVUHWCynig/hqdefault.jpg',
     duration: 160,
     genre: 'R&B'
   },
+  {
+    id: 'die-for-you',
+    youtubeId: 'QLCpqdqeoII',
+    title: 'Die For You',
+    artist: 'The Weeknd',
+    album: 'Starboy',
+    cover: 'https://img.youtube.com/vi/QLCpqdqeoII/hqdefault.jpg',
+    duration: 260,
+    genre: 'R&B'
+  },
+  {
+    id: 'redbone',
+    youtubeId: 'Kp7eSUU2_oc',
+    title: 'Redbone',
+    artist: 'Childish Gambino',
+    album: '"Awaken, My Love!"',
+    cover: 'https://img.youtube.com/vi/Kp7eSUU2_oc/hqdefault.jpg',
+    duration: 326,
+    genre: 'R&B'
+  },
+  {
+    id: 'best-part',
+    youtubeId: 'uQZ2p1_HInY',
+    title: 'Best Part',
+    artist: 'Daniel Caesar ft. H.E.R.',
+    album: 'Freudian',
+    cover: 'https://img.youtube.com/vi/uQZ2p1_HInY/hqdefault.jpg',
+    duration: 209,
+    genre: 'R&B'
+  },
+  {
+    id: 'coffee',
+    youtubeId: '9Z5nM31LzT8',
+    title: 'Coffee',
+    artist: 'Miguel',
+    album: 'Wildheart',
+    cover: 'https://img.youtube.com/vi/9Z5nM31LzT8/hqdefault.jpg',
+    duration: 286,
+    genre: 'R&B'
+  },
 
-  // INDIE & ELECTRONIC
+  // INDIE & CHILL
   {
     id: 'chamber-reflection',
     youtubeId: 'NY8IS0ssnXQ',
     title: 'Chamber of Reflection',
     artist: 'Mac DeMarco',
     album: 'Salad Days',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/NY8IS0ssnXQ/hqdefault.jpg',
+    cover: 'https://img.youtube.com/vi/NY8IS0ssnXQ/hqdefault.jpg',
     duration: 231,
     genre: 'Indie'
   },
+  {
+    id: 'the-less-i-know',
+    youtubeId: 'O28u_78L7Z4',
+    title: 'The Less I Know The Better',
+    artist: 'Tame Impala',
+    album: 'Currents',
+    cover: 'https://img.youtube.com/vi/O28u_78L7Z4/hqdefault.jpg',
+    duration: 218,
+    genre: 'Indie'
+  },
+  {
+    id: 'borderline',
+    youtubeId: '2g5xzll15Lw',
+    title: 'Borderline',
+    artist: 'Tame Impala',
+    album: 'The Slow Rush',
+    cover: 'https://img.youtube.com/vi/2g5xzll15Lw/hqdefault.jpg',
+    duration: 237,
+    genre: 'Indie'
+  },
+  {
+    id: 'sweater-weather',
+    youtubeId: 'GCdwKhTtNNw',
+    title: 'Sweater Weather',
+    artist: 'The Neighbourhood',
+    album: 'I Love You.',
+    cover: 'https://img.youtube.com/vi/GCdwKhTtNNw/hqdefault.jpg',
+    duration: 240,
+    genre: 'Indie'
+  },
+  {
+    id: 'heat-waves',
+    youtubeId: 'mRD0-GxqHVo',
+    title: 'Heat Waves',
+    artist: 'Glass Animals',
+    album: 'Dreamland',
+    cover: 'https://img.youtube.com/vi/mRD0-GxqHVo/hqdefault.jpg',
+    duration: 238,
+    genre: 'Indie'
+  },
+  {
+    id: 'fluorescent-adolescent',
+    youtubeId: 'ma9I9VBKPiw',
+    title: 'Fluorescent Adolescent',
+    artist: 'Arctic Monkeys',
+    album: 'Favourite Worst Nightmare',
+    cover: 'https://img.youtube.com/vi/ma9I9VBKPiw/hqdefault.jpg',
+    duration: 177,
+    genre: 'Indie'
+  },
+
+  // ELECTRONIC & POP
   {
     id: 'get-lucky',
     youtubeId: '5NV6Rdv1a3I',
     title: 'Get Lucky',
     artist: 'Daft Punk',
     album: 'Random Access Memories',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/5NV6Rdv1a3I/hqdefault.jpg',
+    cover: 'https://img.youtube.com/vi/5NV6Rdv1a3I/hqdefault.jpg',
     duration: 248,
     genre: 'Electronic'
   },
@@ -140,7 +273,7 @@ export const MUSIC_DATA = [
     title: 'Starboy',
     artist: 'The Weeknd ft. Daft Punk',
     album: 'Starboy',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/34Na4j8AVgA/hqdefault.jpg',
+    cover: 'https://img.youtube.com/vi/34Na4j8AVgA/hqdefault.jpg',
     duration: 230,
     genre: 'Electronic'
   },
@@ -150,19 +283,9 @@ export const MUSIC_DATA = [
     title: 'Midnight City',
     artist: 'M83',
     album: 'Hurry Up, We\'re Dreaming',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/dX3k_UAnyRE/hqdefault.jpg',
+    cover: 'https://img.youtube.com/vi/dX3k_UAnyRE/hqdefault.jpg',
     duration: 243,
     genre: 'Electronic'
-  },
-  {
-    id: 'the-less-i-know',
-    youtubeId: 'O28u_78L7Z4',
-    title: 'The Less I Know The Better',
-    artist: 'Tame Impala',
-    album: 'Currents',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/O28u_78L7Z4/hqdefault.jpg',
-    duration: 218,
-    genre: 'Indie'
   },
   {
     id: 'after-dark',
@@ -170,19 +293,49 @@ export const MUSIC_DATA = [
     title: 'After Dark',
     artist: 'Mr.Kitty',
     album: 'Time',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/waAlgFq9Xq8/hqdefault.jpg',
+    cover: 'https://img.youtube.com/vi/waAlgFq9Xq8/hqdefault.jpg',
     duration: 257,
     genre: 'Electronic'
   },
   {
-    id: 'borderline',
-    youtubeId: '2g5xzll15Lw',
-    title: 'Borderline',
-    artist: 'Tame Impala',
-    album: 'The Slow Rush',
-    cover: 'https://images.weserv.nl/?url=https://img.youtube.com/vi/2g5xzll15Lw/hqdefault.jpg',
-    duration: 237,
-    genre: 'Indie'
+    id: 'blinding-lights',
+    youtubeId: 'fHI8X4OXW-Q',
+    title: 'Blinding Lights',
+    artist: 'The Weeknd',
+    album: 'After Hours',
+    cover: 'https://img.youtube.com/vi/fHI8X4OXW-Q/hqdefault.jpg',
+    duration: 200,
+    genre: 'Pop'
+  },
+  {
+    id: 'levitating',
+    youtubeId: 'TUVcZfQe-Kw',
+    title: 'Levitating',
+    artist: 'Dua Lipa',
+    album: 'Future Nostalgia',
+    cover: 'https://img.youtube.com/vi/TUVcZfQe-Kw/hqdefault.jpg',
+    duration: 203,
+    genre: 'Pop'
+  },
+  {
+    id: 'stay',
+    youtubeId: 'kTJczUoc26U',
+    title: 'Stay',
+    artist: 'The Kid LAROI & Justin Bieber',
+    album: 'F*CK LOVE 3: OVER YOU',
+    cover: 'https://img.youtube.com/vi/kTJczUoc26U/hqdefault.jpg',
+    duration: 141,
+    genre: 'Pop'
+  },
+  {
+    id: 'as-it-was',
+    youtubeId: 'H5v3kku4y6Q',
+    title: 'As It Was',
+    artist: 'Harry Styles',
+    album: "Harry's House",
+    cover: 'https://img.youtube.com/vi/H5v3kku4y6Q/hqdefault.jpg',
+    duration: 167,
+    genre: 'Pop'
   }
 ];
 
